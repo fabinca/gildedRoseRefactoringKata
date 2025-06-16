0216ceabfd2aade2527b714fbe796a2eb0ebb62e
Carlotta <c.fabian@turbit.de>
2025-06-16 14:17:31 +0200

    first refactor

--- gildedrose/gildedrose.go
+++ gildedrose/gildedrose.go
@@ -7,42 +7,38 @@ type Item struct {
 
 func UpdateQuality(items []*Item) {
 	for i := 0; i < len(items); i++ {
+		if items[i].Name == "Sulfuras, Hand of Ragnaros" {
+			continue
+		}
+		items[i].SellIn = items[i].SellIn - 1
 
 		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
 			if items[i].Quality > 0 {
-				if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-					items[i].Quality = items[i].Quality - 1
-				}
+				items[i].Quality = items[i].Quality - 1
 			}
 		} else {
 			if items[i].Quality < 50 {
 				items[i].Quality = items[i].Quality + 1
 				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].SellIn < 11 {
+					if items[i].SellIn < 10 {
 						if items[i].Quality < 50 {
 							items[i].Quality = items[i].Quality + 1
 						}
 					}
-					if items[i].SellIn < 6 {
+					if items[i].SellIn < 5 {
 						if items[i].Quality < 50 {
 							items[i].Quality = items[i].Quality + 1
 						}
 					}
 				}
 			}
-		}
-
-		if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-			items[i].SellIn = items[i].SellIn - 1
 		}
 
 		if items[i].SellIn < 0 {
 			if items[i].Name != "Aged Brie" {
 				if items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
 					if items[i].Quality > 0 {
-						if items[i].Name != "Sulfuras, Hand of Ragnaros" {
-							items[i].Quality = items[i].Quality - 1
-						}
+						items[i].Quality = items[i].Quality - 1
 					}
 				} else {
 					items[i].Quality = items[i].Quality - items[i].Quality
