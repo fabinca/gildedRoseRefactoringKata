11465fbaa67ff388e069253751ac0deb73e92fb3
Carlotta <c.fabian@turbit.de>
2025-06-16 14:40:21 +0200

    more refactor+ add conjured logic

--- gildedrose/gildedrose.go
+++ gildedrose/gildedrose.go
@@ -1,5 +1,9 @@
 package gildedrose
 
+import (
+	"strings"
+)
+
 type Item struct {
 	Name            string
 	SellIn, Quality int
@@ -12,43 +16,38 @@ func UpdateQuality(items []*Item) {
 		}
 		items[i].SellIn = items[i].SellIn - 1
 
-		if items[i].Name != "Aged Brie" && items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
+		if items[i].Name == "Aged Brie" {
+			items[i].Quality = items[i].Quality + 1
+			if items[i].SellIn < 0 {
+				items[i].Quality = items[i].Quality + 1
+			}
+		} else if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
+			if items[i].SellIn < 0 {
+				items[i].Quality = 0
+			} else if items[i].SellIn < 5 {
+				items[i].Quality = items[i].Quality + 3
+			} else if items[i].SellIn < 10 {
+				items[i].Quality = items[i].Quality + 2
+			} else {
+				items[i].Quality = items[i].Quality + 1
+			}
+		} else if strings.HasPrefix(items[i].Name, "Conjured") {
+			items[i].Quality = items[i].Quality - 2
+			if items[i].SellIn < 0 {
+				items[i].Quality = items[i].Quality - 2
+			}
+		} else {
 			items[i].Quality = items[i].Quality - 1
 			if items[i].SellIn < 0 {
 				items[i].Quality = items[i].Quality - 1
 			}
-			if items[i].Quality < 0 {
-				items[i].Quality = 0
-			}
-
-		} else {
-			if items[i].Quality < 50 {
-				items[i].Quality = items[i].Quality + 1
-				if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-					if items[i].SellIn < 10 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-					if items[i].SellIn < 5 {
-						if items[i].Quality < 50 {
-							items[i].Quality = items[i].Quality + 1
-						}
-					}
-				}
-			}
 		}
 
-		if items[i].SellIn < 0 {
-			if items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-				items[i].Quality = items[i].Quality - items[i].Quality
-			}
-			if items[i].Name == "Aged Brie" {
-				if items[i].Quality < 50 {
-					items[i].Quality = items[i].Quality + 1
-				}
-			}
-
+		if items[i].Quality < 0 {
+			items[i].Quality = 0
+		}
+		if items[i].Quality > 50 {
+			items[i].Quality = 50
 		}
 	}
 }
